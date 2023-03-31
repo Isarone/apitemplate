80cd0aff9cdd8993dc6d610c9adb6b28babd1363
Samuel Arone <samu.arone@gmail.com>
2023-03-31 00:18:40 -0500

    2da Subida corregida

--- webApiTemplate/Controllers/WeatherForecastController.cs
+++ webApiTemplate/Controllers/WeatherForecastController.cs
@@ -3,9 +3,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace ApiTemplate.Controllers
+namespace webApiTemplate.Controllers
 {
     [ApiController]
     [Route("[controller]")]
